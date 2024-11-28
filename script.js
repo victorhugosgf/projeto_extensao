ab12efd98d17c9ea191dfd2d7d6d9f3233f78ca4
Víctor Hugo <128435290+victorhugosgf@users.noreply.github.com>
2024-11-28 17:24:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Seleciona o botão
+const button = document.getElementById('generateScheduleButton');
+
+// Adiciona o evento de clique para redirecionar
+button.addEventListener('click', function() {
+    // Redireciona para a página 'form.html'
+    window.location.href = 'paginas/form.html';
+});
